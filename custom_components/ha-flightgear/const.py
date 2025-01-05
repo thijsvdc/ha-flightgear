9230329a595e5297e9c8f50f455cada26fa70028
thijsvdc <thijsvdc@gmail.com>
2025-01-05 10:14:29 +0100

    Create const.py

---
+++ custom_components/ha-flightgear/const.py
@@ -0,0 +1,15 @@
+"""Constants for the FlightGear integration."""
+from homeassistant.const import Platform
+
+DOMAIN = "ha_flightgear"
+PLATFORMS = [Platform.SENSOR, Platform.CAMERA]
+
+DEFAULT_NAME = "FlightGear"
+DEFAULT_HOST = "localhost"
+DEFAULT_TELNET_PORT = 5500
+DEFAULT_HTTP_PORT = 8080
+DEFAULT_RTSP_PORT = 8554
+
+CONF_TELNET_PORT = "telnet_port"
+CONF_HTTP_PORT = "http_port"
+CONF_RTSP_PORT = "rtsp_port"
